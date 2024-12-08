cff46451e9f4804b74cb711c329d2a3ea6e285c8
AbdelrahmanElzayat <bodyelmajec@gmail.com>
2024-12-08 04:16:47 +0200

    order page responsive

--- src/components/orderComponents/moreOrders/MoreOrders.tsx
+++ src/components/orderComponents/moreOrders/MoreOrders.tsx
@@ -5,7 +5,7 @@ import stapo from "../../../assets/images/logo.svg";
 import style from "./More.module.css";
 const MoreOrders = () => {
   return (
-    <section className="MoreOrders my-4 w-full px-4 py-1 flex justify-between items-center rounded-[14px] bg-gradient-to-b from-[#E4E9F8] via-[#FBEAFE] to-[#FBEAFE]">
+    <section className="MoreOrders my-4 w-full px-4 py-1 flex flex-wrap gap-2 justify-between items-center rounded-[14px] bg-gradient-to-b from-[#E4E9F8] via-[#FBEAFE] to-[#FBEAFE]">
       <div className="contentMore">
         <span
           className={`font-bold text-[#414141] ${style.moreTitle} custom-stroke`}
@@ -18,13 +18,15 @@ const MoreOrders = () => {
           انضم لمزيد الحين و خلى الكل يشوف منتجاتـك
         </p>
       </div>
-      <Image
-        src={stapo}
-        alt="logo"
-        width={200}
-        height={80}
-        style={{ opacity: 0.04 }}
-      />
+      <div className="hidden md:block">
+        <Image
+          src={stapo}
+          alt="logo"
+          width={200}
+          height={80}
+          style={{ opacity: 0.04 }}
+        />
+      </div>
       <div className="btnsMore flex items-center gap-2">
         <button className="bg-[#A78CD3] rounded-lg text-white text-sm py-[6px] px-5 font-normal">
           اشترك في مزيـد
